10e342f603a2cb654cf307e918b0ec9152812222
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-05-26 15:59:21 +0530

    Simplify CORS configuration in server.js
    Replaced specific CORS configuration with a generic one.

--- server.js
+++ server.js
@@ -14,15 +14,7 @@ connectDB();
 const app = express();
 
 
-app.use(
-  cors({
-    origin: [
-      "https://offer-lerrer-frontend-g06ywihpo-sriram-sivas-projects.vercel.app",
-      "http://localhost:5173"
-    ],
-    credentials: true
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
