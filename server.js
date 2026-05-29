4e616b1643a17f30ee6e465a4366ef3203286a93
Sri Ram Siva S <sriramsivasriramsiva1@gmail.com>
2026-05-29 18:30:55 +0530

    Simplify CORS configuration in server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ connectDB();
 const app = express();
 
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
